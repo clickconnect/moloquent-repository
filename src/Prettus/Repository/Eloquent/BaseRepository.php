30c78c0173b8f4610678a268a4b7499ebcca2aca
Folk <tradade@gmail.com>
2016-07-22 22:13:49 +0700

    Update BaseRepository.php

--- src/Prettus/Repository/Eloquent/BaseRepository.php
+++ src/Prettus/Repository/Eloquent/BaseRepository.php
@@ -6,7 +6,7 @@ use Exception;
 use Illuminate\Container\Container as Application;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Model;
 use Illuminate\Support\Collection;
 use Prettus\Repository\Contracts\CriteriaInterface;
 use Prettus\Repository\Contracts\Presentable;
